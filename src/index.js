370485b51dd1442ebe6a8ccbb521fc98b586781d
Tamilo4ka <sone4ko2019@gmail.com>
2022-08-31 12:52:58 +0300

    Added the link

--- src/index.js
+++ src/index.js
@@ -16,5 +16,16 @@ root.render(
         <h2 id="weather">{""}</h2>
       </div>
     </div>
+    <small>
+      {" "}
+      <a
+        href="https://github.com/Tamilo4ka/weather-react"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open-sourced on GitHub{" "}
+      </a>
+      by Tamila{" "}
+    </small>
   </StrictMode>
 );
